937b1692d6c6134c9ebc4580c584e027669951b6
Isaac Urbina <iurbinacantu@gmail.com>
2016-03-10 12:09:18 -0500

    AlarmManager added

--- app/src/main/java/com/mobileappsco/training/jobschedulerapp/JobSchedulerService.java
+++ app/src/main/java/com/mobileappsco/training/jobschedulerapp/JobSchedulerService.java
@@ -2,8 +2,10 @@ package com.mobileappsco.training.jobschedulerapp;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.widget.Toast;
 
 public class JobSchedulerService extends JobService {
@@ -14,10 +16,14 @@ public class JobSchedulerService extends JobService {
 
         @Override
         public boolean handleMessage( Message msg ) {
-            Toast.makeText( getApplicationContext(),
+            /*Toast.makeText( getApplicationContext(),
                     "JobService task running", Toast.LENGTH_SHORT )
                     .show();
-            jobFinished( (JobParameters) msg.obj, false );
+            jobFinished( (JobParameters) msg.obj, false );*/
+            Log.d("MYTAG", "handleMessage() opening app");
+            Intent i = new Intent(getBaseContext(), MainActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
             return true;
         }
 
--- app/src/main/java/com/mobileappsco/training/jobschedulerapp/MainActivity.java
+++ app/src/main/java/com/mobileappsco/training/jobschedulerapp/MainActivity.java
@@ -1,32 +1,55 @@
 package com.mobileappsco.training.jobschedulerapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private JobScheduler mJobScheduler;
     private final int JOB_ID = 123;
+    private AlarmManager mAlarmManager;
+    JobInfo.Builder builder;
+    private Intent alarmIntent;
+    private PendingIntent pendingIntent;
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MYTAG", "onStop() scheduling app reopening every 5 seconds");
+        if( mJobScheduler.schedule( builder.build() ) <= 0 ) {
+            //If something goes wrong
+            Toast.makeText(this, "Error scheduling job", Toast.LENGTH_SHORT).show();
+            Log.e("MYTAG", "onStop() ERROR scheduling app reopening");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // JOBSCHEDULER
         mJobScheduler = (JobScheduler)
                 getSystemService( Context.JOB_SCHEDULER_SERVICE );
-        JobInfo.Builder builder = new JobInfo.Builder( JOB_ID,
+        builder = new JobInfo.Builder( JOB_ID,
                 new ComponentName( getPackageName(),
                         JobSchedulerService.class.getName() ) );
-        builder.setPeriodic(3000);
+        builder.setPeriodic(5000);
 
-        if( mJobScheduler.schedule( builder.build() ) <= 0 ) {
-            //If something goes wrong
-            Toast.makeText(this, "Error scheduling job", Toast.LENGTH_SHORT).show();
-        }
+        // ALARMMANAGER
+        mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
+        int interval = 5000;
+        mAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
     }
 }
